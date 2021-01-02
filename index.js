b96b7a90305fc944351c5931dc40f3595aee7e72
Deeptiman Panda <deeptimanpanda@Deeptimans-Mac-mini.local>
2021-01-03 00:45:13 +0530

    Deploying First

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+})
+
+const PORT = process.env.PORT | 4000;
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}/`));
